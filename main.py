2ab480065f5d39ad64b8ce67b010b9d4af12909d
johnb254 <46943648+johnb254@users.noreply.github.com>
2025-04-04 19:13:43 -0400

    Add function documentation

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ import sys
 import json
 from datetime import datetime
 
+
 def read_data_file():
+    """
+    Gathers the task list and its corresponding data from the json file.
+    
+    Returns:
+        list[dict]: Returns the lists of tasks currently contained within the file. If no file is 
+        found or no data is within the file, returns an empty list.
+    """
     try:
         file = open("data.json", "r")
         tasks = json.load(file)
@@ -16,9 +24,16 @@ def read_data_file():
     return []
 
 def add_task(description: str):
+    """
+    Adds a task to the tasks contained in the data file.
+    
+    Args:
+        description (str): The description of the task.
+    """
     tasks: list[dict] = read_data_file()
     
     with open("data.json", "w") as file:
+        # The task id will always be 1 + the id of the most recently created task.
         task_id = 1
         if len(tasks) > 0:
             task_id = max(tasks, key=lambda x: x["id"])["id"] + 1
@@ -33,13 +48,29 @@ def add_task(description: str):
         print("Task added successfully (ID: ", task_id, ")")
             
 def find(list: list, value: int):
+    """
+    Finds the index of task with a given id.
+    
+    Args:
+        list (int): The list of tasks.
+        value (int): The id of the target task.
+        
+    Returns:
+        int: The index of the target task.
+    """
     for i, dict in enumerate(list):
-        #print ("", i, "", dict, "",type(dict['id']), "", type(value))
         if int(dict['id']) == int(value):
             return i
     return -1
 
 def update_task_des(id: int, description: str):
+    """
+    Updates the given task with a new description.
+    
+    Args:
+        id (int): Id of the task to be updated.
+        description (str): The description to replace the old one.
+    """
     tasks: list[dict] = read_data_file()
     
     if len(tasks) == 0:
@@ -51,6 +82,7 @@ def update_task_des(id: int, description: str):
         print("Data not found")
         return
     
+    # Update the description and the update time
     tasks[target].update({
             "description" : description,
             "updatedAt" : datetime.now().strftime("%H:%M:%S %m-%d-%Y"),
@@ -60,6 +92,13 @@ def update_task_des(id: int, description: str):
         print("Successfully updated description")
             
 def update_task_status(id: int, status: str):
+    """
+    Updates the given task with a new status.
+    
+    Args:
+        id (int): Id of the task to be updated.
+        status (str): The status to replace the old one. Should be "todo", "in-progress", or "done".
+    """
     tasks: list[dict] = read_data_file()
     
     if len (tasks) == 0:
@@ -71,6 +110,7 @@ def update_task_status(id: int, status: str):
         print("Data not found")
         return
     
+    # Update status and update time
     tasks[target].update({
         "status" : status,
         "updatedAt" : datetime.now().strftime("%H:%M:%S %m-%d-%Y"),
@@ -80,6 +120,12 @@ def update_task_status(id: int, status: str):
         print("Successfully updated status")
 
 def delete_task(id: int):
+    """
+    Deletes a task from the data file.
+    
+    Args:
+        id (int): The id of the task to be removed.
+    """
     tasks: list[dict] = read_data_file()
     
     if len (tasks) == 0:
@@ -98,6 +144,13 @@ def delete_task(id: int):
         print("Successfully deleted task")
 
 def list_tasks(options = "all"):
+    """
+    Lists the tasks within the data file. Can be filtered by status.
+    
+    Args:
+        options (str): A filter for the tasks to show. Can be set to "todo", "in-progress", "done",
+        or "all". Set to "all" by default.
+    """
     tasks: list[dict] = read_data_file()
     
     if len (tasks) == 0:
